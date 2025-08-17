01a887d39b9cf96c2b5bf05fcaa2b774e4146b91
VishalKammari <vishalkodiganti@gmail.com>
2025-08-17 16:53:52 +0530

    added favicon

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const path = require("path");
+const favicon = require("serve-favicon");
+
 const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
+
+app.use(favicon(path.join(__dirname, "favicon.png")));
+
 mongoose.connect('mongodb+srv://vishal:vishal2102@cluster0.7dw8zod.mongodb.net/shortly?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log("✅ Connected to MongoDB Atlas"))
   .catch(err => console.error("❌ MongoDB Connection Error:", err));
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:false}));
 
 app.get('/', async(req, res) => {
